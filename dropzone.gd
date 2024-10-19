536a84d1397bc8ff4a56bd01682ef79823003f46
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-10-19 13:41:06 +0800

    drag n drop implemented

---
+++ dropzone.gd
@@ -0,0 +1,12 @@
+extends Marker2D
+
+func _draw():
+	draw_circle(Vector2.ZERO, 85, Color.LIGHT_SALMON)
+	
+func _select():
+	for child in get_tree().get_nodes_in_group("zone"):
+		child.deselect()
+	modulate = Color.WEB_MAROON
+		
+func deselect():
+	modulate = Color.WHITE
